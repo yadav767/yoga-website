e5249c3d3c8f603580966a131f4d6620cd86ba7e
yadav767 <yyadavaayush767@gmail.com>
2026-02-25 23:19:06 +0530

    updated

--- server/src/services/mail.service.js
+++ server/src/services/mail.service.js
@@ -1,11 +1,13 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.USER_GMAIL,
-        pass: process.env.APP_PASSWORD 
-    },
+        pass: process.env.APP_PASSWORD
+    }
 });
 
 // Verify the connection configuration
